ecc189e18db9f13e8c891403726871836cba38ca
Jonathan Araujo <jonathan@wicadu.com>
2023-07-24 15:40:14 -0400

    Git init

---
+++ windows/financial_indicators/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::financial_indicators::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::financial_indicators::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
