2770460077a81163715629e0a3ea81ee8ee8b909
Jonathan Araujo <jonathan@wicadu.com>
2023-07-26 11:43:24 -0400

    Add ErrorNotice atom

---
+++ components/atoms/ErrorNotice.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+function ErrorNotice() {
+  return (
+    <Text>Oops, an error has happened. :(</Text>
+  )
+}
+
+
+export default ErrorNotice
