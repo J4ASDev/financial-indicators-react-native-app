c1453bb8852f2bbca19fabd87f4ccf6ff303cd8e
Jonathan Araujo <jonathan@wicadu.com>
2023-07-26 11:43:42 -0400

    Integrate Navigation and screens into App

--- App.tsx
+++ App.tsx
@@ -1,118 +1,55 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useMemo } from 'react'
+import { NavigationContainer, } from '@react-navigation/native'
+import { NativeStackNavigationOptions, createNativeStackNavigator } from '@react-navigation/native-stack'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { colors } from './consts/theme'
+import Home from './containers/Home'
+import ValuesByIndicator from './containers/ValuesByIndicator'
+import IndicatorDetails from './containers/IndicatorDetails'
+import HeaderBack from './components/atoms/HeaderBack'
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Stack = createNativeStackNavigator()
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+function App() {
+  const stackOptions: NativeStackNavigationOptions = useMemo(() => ({
+    contentStyle: {
+      backgroundColor: colors.BACKGROUND
+    }
+  }), [])
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+  const homeOptions: NativeStackNavigationOptions = useMemo(() => ({
+    headerShown: false,
+  }), [])
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const screenOptions: NativeStackNavigationOptions = useMemo(() => ({
+    header: () => (
+      <GestureHandlerRootView>
+        <HeaderBack />
+      </GestureHandlerRootView>
+    ),
+    title: '',
+    headerShadowVisible: false,
+  }), [])
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>Apdasdap.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+    <NavigationContainer >
+      <Stack.Navigator screenOptions={stackOptions} initialRouteName='Home'>
+        <Stack.Screen name='Home' options={homeOptions} component={Home} />
+        <Stack.Screen
+          name='ValuesByIndicator'
+          options={screenOptions}
+          component={ValuesByIndicator}
+        />
+        <Stack.Screen
+          name='IndicatorDetails'
+          options={screenOptions}
+          component={IndicatorDetails}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+export default App
