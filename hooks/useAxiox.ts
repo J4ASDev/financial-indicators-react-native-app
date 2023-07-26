f5a0823975189b572f1e5a2bb72ef2b4e44d7ed3
Jonathan Araujo <jonathan@wicadu.com>
2023-07-26 11:16:16 -0400

    Add loader to useAxios

--- hooks/useAxiox.ts
+++ hooks/useAxiox.ts
@@ -4,31 +4,37 @@ import axios, { AxiosRequestConfig } from 'axios'
 type Response<D> = {
   data: {} | D;
   error: null | Error;
+  loading: boolean
 }
 
-export type HookResponse<D> = [D | {}, Error | null]
+export type HookResponse<D> = [D | {}, Error | null, boolean]
 
 export default function useAxios<D>(url: string, options?: AxiosRequestConfig): HookResponse<D> {
   const [data, setData] = useState<Response<D>['data']>({})
   const [error, setError] = useState<Response<D>['error']>(null)
-
+  const [loading, setLoading] = useState<Response<D>['loading']>(false)
+  
   useEffect(() => {
+    setLoading(true);
+
     (async () => {
       await axios.get(url, options)
         .then(async ({ data }) => {
           setData(data)
           setError(null)
+          setLoading(false)
 
           return { data, error: null }
         })
         .catch((error) => {
           setData({})
           setError(error)
-  
+          setLoading(false)
+
           return { data: {}, error }
         })
     })()
   }, [])
 
-  return [data, error]
+  return [data, error, loading]
 }
